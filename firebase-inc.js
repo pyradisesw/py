3acc81c0dfa6597c02339a40aae359949ed335c6
kwh703 <k.w.harris@gmail.com>
2020-12-07 21:05:03 -0500

    Create firebase-inc.js

---
+++ firebase-inc.js
@@ -0,0 +1,19 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCtZ-9WQyxVF2D0Naf6c6_LQwUPGq1goJc",
+    authDomain: "pyradise-e0e9f.firebaseapp.com",
+    projectId: "pyradise-e0e9f",
+    storageBucket: "pyradise-e0e9f.appspot.com",
+    messagingSenderId: "36758105370",
+    appId: "1:36758105370:web:b1e43961f83406afa810a3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
