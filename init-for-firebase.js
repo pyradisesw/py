61cb721e4b9fab2bc77754a821ad1e5ff955ec9e
kwh703 <k.w.harris@gmail.com>
2020-12-07 21:21:18 -0500

    Create init-for-firebase.js

---
+++ init-for-firebase.js
@@ -0,0 +1,17 @@
+<body>
+  <!-- Insert these scripts in BODY at the bottom of the HTML, AFTER the app config but before you use any Firebase services -->
+
+  <!-- Firebase App (the core Firebase SDK) is always required and must be listed first -->
+  <script src="/__/firebase/8.1.2/firebase-app.js"></script>
+
+  <!-- If you enabled Analytics in your project, add the Firebase SDK for Analytics -->
+  <script src="/__/firebase/8.1.2/firebase-analytics.js"></script>
+
+  <!-- Add Firebase products that you want to use -->
+  <script src="/__/firebase/8.1.2/firebase-auth.js"></script>
+  <script src="/__/firebase/8.1.2/firebase-firestore.js"></script>
+  <!-- Previously loaded Firebase SDKs -->
+
+  <!-- Initialize Firebase -->
+  <script src="/__/firebase/init.js"></script>
+</body>
